a6c9b81583557f7f0f701f10566ec4fa68359643
JC <jcmontes95@gmail.com>
2017-06-15 21:23:56 -0300

    cambios en el modelo

--- escuela/models.py
+++ escuela/models.py
@@ -1,56 +1,51 @@
 from django.db import models
+import datetime as dt
 
 
 # Create your models here.
-class Provincia(models.Model):
-    nombre = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.nombre
-
 
 class Alumno(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
+    nombre = models.CharField(blank=True, null=True, max_length=50)
+    apellido = models.CharField(blank=True, null=True, max_length=50)
     numero_doc = models.CharField(max_length=15)
     TIPOS_DOC = (
         ('DNI', 'Documento Nacional de Identidad'),
         ('LE', 'Libreta de enrolamiento'),
         ('PS', 'Pasaporte')
     )
-    tipo_doc = models.CharField(max_length=3, choices=TIPOS_DOC)
-    fecha_nacimiento = models.DateField()
-    telefono = models.CharField(max_length=12)
-    domicilio = models.CharField(max_length=50)
-    cp = models.CharField(max_length=8)
-    localidad = models.CharField(max_length=50)
-    provincia = models.ForeignKey(Provincia)
-    anio_ingreeo = models.CharField(max_length=8)
-    libro_matriz = models.CharField(max_length=40)
-    anio_egreso = models.IntegerField(max_length=8)
+    tipo_doc = models.CharField(max_length=3, choices=TIPOS_DOC, default=TIPOS_DOC[0][0])
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    telefono = models.CharField(blank=True, null=True, max_length=12)
+    domicilio = models.CharField(blank=True, null=True, max_length=50)
+    cp = models.CharField(blank=True, null=True, max_length=8)
+    localidad = models.CharField(blank=True, null=True, max_length=50)
+    provincia = models.CharField(blank=True, null=True, max_length=40)
+    anio_ingreeo = models.CharField(blank=True, null=True, max_length=8)
+    libro_matriz = models.CharField(blank=True, null=True, max_length=40)
+    anio_egreso = models.IntegerField(blank=True, null=True, max_length=8)
     GENEROS = (
         ('M', 'Masculino'),
         ('F', 'Femenino'),
         ('O', 'Otro')
     )
-    genero = models.CharField(max_length=1, choices=GENEROS)
-    observaciones = models.TextField()
-    escuela_origen = models.CharField(max_length=40)
-    cooperadora_paga = models.BooleanField()
-    celular = models.CharField(max_length=12)
-    lugar_nacimiento = models.CharField(max_length=40)
-    tipo_escuela_origen = models.CharField(max_length=40) #TODO: agregar choices
-    pais = models.CharField(max_length=40)
-    partida_nacimiento = models.BooleanField()
-    fotocopia_dni = models.BooleanField()
-    certificado_estudios = models.BooleanField()
+    genero = models.CharField(blank=True, null=True, max_length=1, choices=GENEROS)
+    observaciones = models.TextField(blank=True, null=True)
+    escuela_origen = models.CharField(blank=True, null=True, max_length=40)
+    cooperadora_paga = models.BooleanField(default=False)
+    celular = models.CharField(blank=True, null=True, max_length=12)
+    lugar_nacimiento = models.CharField(blank=True, null=True, max_length=40)
+    tipo_escuela_origen = models.CharField(blank=True, null=True, max_length=40) #TODO: agregar choices
+    pais = models.CharField(blank=True, null=True, max_length=40)
+    partida_nacimiento = models.BooleanField(default=False)
+    fotocopia_dni = models.BooleanField(default=False)
+    certificado_estudios = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nombre
 
 
 class Plan(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(blank=True, null=True, max_length=50)
 
     def __str__(self):
         return self.nombre
@@ -67,15 +62,15 @@ class Curso(models.Model):
 class Inscripcion(models.Model):
     alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE, related_name="inscripciones")
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE, related_name="inscripciones")
-    estado = models.CharField(max_length=4) # TODO: agregar choices
-    estado_pase = models.CharField(max_length=4) # TODO: agregar choices
+    estado = models.CharField(blank=True, null=True, max_length=4) # TODO: agregar choices
+    estado_pase = models.CharField(blank=True, null=True, max_length=4) # TODO: agregar choices
 
 
 class Materia(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(blank=True, null=True, max_length=50)
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE, related_name="materias")
-    horas = models.IntegerField(max_length=8)
-    activa = models.BooleanField()
+    horas = models.IntegerField(blank=True, null=True, max_length=8)
+    activa = models.BooleanField(default=True)
 
     def __str__(self):
         return self.nombre
@@ -83,35 +78,35 @@ class Materia(models.Model):
 
 class Docente(models.Model):
     materias = models.ManyToManyField('Materia', blank=True, related_name="docentes")
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
+    nombre = models.CharField(blank=True, null=True, max_length=50)
+    apellido = models.CharField(blank=True, null=True, max_length=50)
     numero_doc = models.CharField(max_length=15)
     TIPOS_DOC = (
         ('DNI', 'Documento Nacional de Identidad'),
         ('LE', 'Libreta de enrolamiento'),
         ('PS', 'Pasaporte')
     )
-    tipo_doc = models.CharField(max_length=3, choices=TIPOS_DOC)
-    fecha_nacimiento = models.DateField()
-    telefono = models.CharField(max_length=12)
-    domicilio = models.CharField(max_length=50)
-    cp = models.CharField(max_length=8)
-    localidad = models.CharField(max_length=50)
-    provincia = models.ForeignKey(Provincia)
-    fecha_ingreso = models.DateField()
-    fecha_egreso = models.DateField()
-    libro_matriz = models.CharField(max_length=40)
-    anio_egreso = models.IntegerField(max_length=8)
+    tipo_doc = models.CharField(max_length=3, choices=TIPOS_DOC, default=TIPOS_DOC[0][0])
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    telefono = models.CharField(blank=True, null=True, max_length=12)
+    domicilio = models.CharField(blank=True, null=True, max_length=50)
+    cp = models.CharField(blank=True, null=True, max_length=8)
+    localidad = models.CharField(blank=True, null=True, max_length=50)
+    provincia = models.CharField(blank=True, null=True, max_length=40)
+    fecha_ingreso = models.DateField(blank=True, null=True)
+    fecha_egreso = models.DateField(blank=True, null=True)
+    libro_matriz = models.CharField(blank=True, null=True, max_length=40)
+    anio_egreso = models.IntegerField(blank=True, null=True, max_length=8)
     GENEROS = (
         ('M', 'Masculino'),
         ('F', 'Femenino'),
         ('O', 'Otro')
     )
-    genero = models.CharField(max_length=1, choices=GENEROS)
-    activo = models.BooleanField()
-    licencia = models.BooleanField()
-    baja = models.BooleanField()
-    calificacion = models.CharField(max_length=40)
+    genero = models.CharField(blank=True, null=True, max_length=1, choices=GENEROS)
+    activo = models.BooleanField(default=True)
+    licencia = models.BooleanField(default=False)
+    baja = models.BooleanField(default=False)
+    calificacion = models.CharField(blank=True, null=True, max_length=40)
 
     def __str__(self):
         return self.nombre
@@ -119,8 +114,8 @@ class Docente(models.Model):
 
 class Calificacion(models.Model):
     nota = models.FloatField(default=1)
-    fecha = models.DateField()
-    tipo = models.CharField(max_length=4) #TODO: agregar choices
+    fecha = models.DateField(blank=True, null=True)
+    tipo = models.CharField(blank=True, null=True, max_length=4) #TODO: agregar choices
     alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE, related_name="calificaciones")
     materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name="calificaciones")
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE, related_name="calificaciones")
--- static/js/app.js
+++ static/js/app.js
@@ -4,20 +4,21 @@
 (function () {
     'use strict';
 
-    angular.module('app',['ngRoute'])
-        .config(['$routeProvider', config]);
+    var app = angular.module('app',['ngRoute']);
+
+    app.config(['$routeProvider', config]);
 
     function config($routeProvider) {
 
         $routeProvider
-            .when('/', {
-                templateUrl: '/static/html/feed.html',
-                controller: 'FeedController'
-            })
-            .when('/post/:slug', {
-                templateUrl: '/static/html/post.html',
-                controller: 'PostController'
-            })
+          //  .when('/', {
+          //      templateUrl: '/static/html/feed.html',
+          //      controller: 'FeedController'
+          //  })
             .otherwise('/');
     }
+
+    app.controller("DropdownCtrl", function(){
+
+    });
 })();
