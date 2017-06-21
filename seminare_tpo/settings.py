64e5ce8847089d96b848d41090d016afef4e3123
JC <jcmontes95@gmail.com>
2017-06-21 20:56:00 -0300

    production settings

--- seminare_tpo/settings.py
+++ seminare_tpo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=h@q#y#ls0o&wyt8lg9s0qab+)-+cn@)ho78tin(iu&5oder*('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ('*')
 
 
 # Application definition
